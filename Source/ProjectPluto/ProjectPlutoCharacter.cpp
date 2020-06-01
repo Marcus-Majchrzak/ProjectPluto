517f25155e78f6c2bfe61fada09fe6500f6c96d0
Marcus-Majchrzak <marcusmajchrzak@gmail.com>
2020-06-01 11:01:39 +1000

    Modular Characters and First Person Mode
    Same as previous commit - didn't push everything

--- Source/ProjectPluto/ProjectPlutoCharacter.cpp
+++ Source/ProjectPluto/ProjectPlutoCharacter.cpp
@@ -8,6 +8,9 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "Engine/Engine.h"
+#include "ProjectPlutoProjectile.h"
 
 //////////////////////////////////////////////////////////////////////////
 // AProjectPlutoCharacter
@@ -21,9 +24,13 @@ AProjectPlutoCharacter::AProjectPlutoCharacter()
 	BaseTurnRate = 45.f;
 	BaseLookUpRate = 45.f;
 
-	// Don't rotate when the controller rotates. Let that just affect the camera.
+	// Initialize the player's Health
+	MaxHealth = 100.0f;
+	CurrentHealth = MaxHealth;
+
+	// Only rotate the character left/right when the camera moves, not up/down
 	bUseControllerRotationPitch = false;
-	bUseControllerRotationYaw = false;
+	bUseControllerRotationYaw = true;
 	bUseControllerRotationRoll = false;
 
 	// Configure character movement
@@ -32,11 +39,17 @@ AProjectPlutoCharacter::AProjectPlutoCharacter()
 	GetCharacterMovement()->JumpZVelocity = 600.f;
 	GetCharacterMovement()->AirControl = 0.2f;
 
+	// Set up camera constants
+	inFirstPerson = false;
+	thirdPersonCameraArmLength = 300.0f;
+	firstPersonCameraArmLength = 0.0f;
+
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
+	CameraBoom->TargetArmLength = thirdPersonCameraArmLength; // The camera follows at this distance behind the character	
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+	CameraBoom->SetRelativeLocation(FVector(10.0f, 0.0f, 70.0f));
 
 	// Create a follow camera
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
@@ -45,6 +58,12 @@ AProjectPlutoCharacter::AProjectPlutoCharacter()
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
+
+	//Initialize projectile class
+	ProjectileClass = AProjectPlutoProjectile::StaticClass();
+	//Initialize fire rate
+	FireRate = 0.25f;
+	bIsFiringWeapon = false;
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -74,6 +93,12 @@ void AProjectPlutoCharacter::SetupPlayerInputComponent(class UInputComponent* Pl
 
 	// VR headset functionality
 	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AProjectPlutoCharacter::OnResetVR);
+
+	// Handle firing projectiles
+	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AProjectPlutoCharacter::StartFire);
+
+	// Handle Camera Perspective Switch
+	PlayerInputComponent->BindAction("PerspectiveSwitch", IE_Pressed, this, &AProjectPlutoCharacter::PerspectiveSwitch);
 }
 
 
@@ -131,4 +156,115 @@ void AProjectPlutoCharacter::MoveRight(float Value)
 		// add movement in that direction
 		AddMovementInput(Direction, Value);
 	}
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////
+// Replicated Properties
+
+void AProjectPlutoCharacter::GetLifetimeReplicatedProps(TArray <FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	//Replicate current health.
+	DOREPLIFETIME(AProjectPlutoCharacter, CurrentHealth);
+}
+
+void AProjectPlutoCharacter::OnHealthUpdate()
+{
+	//Client-specific functionality
+	if (IsLocallyControlled())
+	{
+		FString healthMessage = FString::Printf(TEXT("You now have %f health remaining."), CurrentHealth);
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, healthMessage);
+
+		if (CurrentHealth <= 0)
+		{
+			FString deathMessage = FString::Printf(TEXT("You have been killed."));
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, deathMessage);
+		}
+	}
+
+	//Server-specific functionality
+	if (GetLocalRole() == ROLE_Authority)
+	{
+		FString healthMessage = FString::Printf(TEXT("%s now has %f health remaining."), *GetFName().ToString(), CurrentHealth);
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, healthMessage);
+	}
+
+	//Functions that occur on all machines. 
+	/*
+		Any special functionality that should occur as a result of damage or death should be placed here.
+	*/
+}
+
+void AProjectPlutoCharacter::OnRep_CurrentHealth()
+{
+	OnHealthUpdate();
+}
+
+void AProjectPlutoCharacter::SetCurrentHealth(float healthValue)
+{
+	if (GetLocalRole() == ROLE_Authority)
+	{
+		CurrentHealth = FMath::Clamp(healthValue, 0.f, MaxHealth);
+		OnHealthUpdate();
+	}
+}
+float AProjectPlutoCharacter::TakeDamage(float DamageTaken, struct FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	float damageApplied = CurrentHealth - DamageTaken;
+	SetCurrentHealth(damageApplied);
+	return damageApplied;
+}
+
+void AProjectPlutoCharacter::StartFire()
+{
+	if (!bIsFiringWeapon)
+	{
+		bIsFiringWeapon = true;
+		UWorld* World = GetWorld();
+		World->GetTimerManager().SetTimer(FiringTimer, this, &AProjectPlutoCharacter::StopFire, FireRate, false);
+		HandleFire();
+	}
+}
+
+void AProjectPlutoCharacter::StopFire()
+{
+	bIsFiringWeapon = false;
+}
+
+
+void AProjectPlutoCharacter::HandleFire_Implementation()
+{
+	FVector spawnLocation = GetActorLocation() + (GetControlRotation().Vector() * 100.0f) + (GetActorUpVector() * 50.0f);
+	FRotator spawnRotation = GetControlRotation();
+
+	FActorSpawnParameters spawnParameters;
+	spawnParameters.Instigator = GetInstigator();
+	spawnParameters.Owner = this;
+
+	AProjectPlutoProjectile* spawnedProjectile = GetWorld()->SpawnActor<AProjectPlutoProjectile>(spawnLocation, spawnRotation, spawnParameters);
+}
+
+void AProjectPlutoCharacter::PerspectiveSwitch()
+{
+	USpringArmComponent* Camera = GetCameraBoom();
+	USkeletalMeshComponent* CharacterMesh = GetMesh();
+
+	FName HeadBone = FName(TEXT("Head"));
+	//Change Camera Length as appropriate
+	if (inFirstPerson) {
+		CharacterMesh->UnHideBoneByName(HeadBone);
+		Camera->TargetArmLength = thirdPersonCameraArmLength;
+	}
+	else {
+		CharacterMesh->HideBoneByName(HeadBone, PBO_MAX);
+		Camera->TargetArmLength = firstPersonCameraArmLength;
+	}
+	//Swap from third to first person
+	inFirstPerson = !inFirstPerson;
+
+	
 }
